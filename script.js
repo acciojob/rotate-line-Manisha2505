8e64a6fba3456e0f6f6fa1b50b2487893ce6e249
Manisha2505 <manishachandra.25@gmail.com>
2025-02-02 02:29:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
-// //your JS code here. If required.
-// let angle = 0;
+ let angle = 0;
+        const line = document.getElementById('line');
 
-//         const line = document.getElementById('line');
+        function rotateLine() {
+            angle += 2;
+            line.style.transform = `rotate(${angle}deg)`;
+            setTimeout(rotateLine, 20);
+        }
 
-//         function rotateLine() {
-
-//             angle += 2;
-
-//             line.style.transform = rotate(${angle}deg);
-
-//             setTimeout(rotateLine, 20);
-
-//         }
-
-//         rotateLine();
+        rotateLine();
